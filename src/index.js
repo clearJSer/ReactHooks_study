8514fe204f9afc08b030527ae4ea5567e87477c5
lakiX <784335750@qq.com>
2022-06-20 00:01:02 +0800

    useRef useMemo study

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 // import Example1 from './Example1';
 // import Example2 from './Example2';
 // import Example2 from './ReducerDemo';
-import Example2 from './Example6';
+import Example3 from './Example6';
+import Example2 from './useMemo-example';
+import Example8 from './useRef-example'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Example2 />
+    <>
+        <Example2 />
+        <Example3 />
+        <Example8 />
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
