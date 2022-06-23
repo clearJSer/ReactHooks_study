560d2168a8c1947d1469ab78ba569cd5fffc832a
lakiliu <lakiliu@tencent.com>
2022-06-23 12:52:59 +0800

    aa

--- src/Example6/Buttons.js
+++ src/Example6/Buttons.js
@@ -6,7 +6,7 @@ function Buttons() {
         <div>
             <button onClick={() => {dispatch({type:UPDATE_COLOR, color:'red'})}}>红色</button>
             <button onClick={() => {dispatch({type:UPDATE_COLOR, color:'yellow'})}}>黄色</button>
-        </div >
+        </div>
     )
 }
 
