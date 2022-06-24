4b5736a0520e122587c0a0679465879a7b7936c3
lakiliu <lakiliu@tencent.com>
2022-06-24 23:53:39 +0800

    update

--- src/Example6/Color.js
+++ src/Example6/Color.js
@@ -1,21 +1,19 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer } from 'react';
 export const ColorContext = createContext({});
 export const UPDATE_COLOR = 'UPDATE_COLOR';
 const reducer = (state, action) => {
-    switch (action.type) {
-        case UPDATE_COLOR:
-            return action.color
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case UPDATE_COLOR:
+      return action.color;
+    default:
+      return state;
+  }
+};
+export default function Color(props) {
+  const [color, dispatch] = useReducer(reducer, 'blue');
+  return (
+    <>
+      <ColorContext.Provider value={{ color, dispatch }}>{props.children}</ColorContext.Provider>
+    </>
+  );
 }
-export default function (props) {
-    const [color, dispatch] = useReducer(reducer, 'blue')
-    return (
-        <>
-            <ColorContext.Provider value={{ color, dispatch }}>
-                {props.children}
-            </ColorContext.Provider>
-        </>
-    )
-} 
